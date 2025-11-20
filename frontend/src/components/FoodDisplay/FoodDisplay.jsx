9d00d7568342aad2bac63cfc82d5b33e98bbdf71
namanagarwal2k <naman.aggarwal@fiftyfivetech.io>
2025-11-20 16:13:20 +0530

    Fix FoodDisplay props

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,17 +3,24 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = ({category}) => {
-
-  const {food_list} = useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext)
 
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item,index)=>{
-          return <FoodItem key={index} id={item._id} name={item._name} description={item._description} price={item._price} image={item._image}/>
-        })}
+        {food_list.map((item, index) => (
+          <FoodItem
+            key={index}
+            id={item._id}
+            name={item.name}           
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+
+        ))}
       </div>
     </div>
   )
