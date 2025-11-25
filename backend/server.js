0b8b76a4655fa75ca82009b7ac2c8d06d2bc0b21
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-11-25 15:23:21 +0530

    Integrate food routes into server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use(cors())
 
 connectDB();
+
+app.use("/api/food", foodRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working")
