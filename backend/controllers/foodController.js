5fea9f505a6a3993aeef70c9eeee32b6513d42b9
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-11-26 14:43:56 +0530

    Add listFood, improve foodController

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -22,4 +22,14 @@ const addFood = async (req, res) => {
   }
 }
 
-export { addFood }
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods })
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" })
+  }
+}
+
+export { addFood, listFood }
