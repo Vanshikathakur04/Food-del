6e9d771f667a68248db804f6f86ebc6a5872d029
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-02 15:02:30 +0530

    Add user routes and dotenv config

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
-
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
 
 
 const app = express()
@@ -15,6 +16,7 @@ connectDB();
 
 app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working")
