9f2fe6dc043aaff3bbd71fea96ac356fb330d359
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-02 15:28:07 +0530

    Implement loginUser and registerUser with JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,30 @@ import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 import validator from "validator"
 
-const loginUser = async (req, res) => { }
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User Doesn't exists" })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid credentials" })
+
+    }
+
+    const token = createToken(user._id)
+    res.json({ success: true, token })
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" })
+  }
+}
 
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET)
