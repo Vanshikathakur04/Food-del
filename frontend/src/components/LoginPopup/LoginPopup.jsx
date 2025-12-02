ec7b5b505eb7f9b55d2c9f1f5fb75211cf057a29
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-02 16:06:59 +0530

    Add state handling for login/signup inputs

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -5,6 +5,17 @@ import { assets } from '../../assets/assets'
 const LoginPopup = ({ setShowLogin }) => {
 
   const [currState, setCurrState] = useState("Login")
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({ ...data, [name]: value }))
+  }
 
   return (
     <div className='login-popup'>
@@ -14,9 +25,9 @@ const LoginPopup = ({ setShowLogin }) => {
           <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className="login-popup-inputs">
-          {currState === "Login" ? <></> : <input type="text" placeholder='Your name' required />}
-          <input type="email" placeholder='Your email' required />
-          <input type="password" placeholder='Password' required />
+          {currState === "Login" ? <></> : <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required />}
+          <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' required />
+          <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
         </div>
         <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
         <div className="login-popup-condition">
