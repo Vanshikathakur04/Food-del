9229027d3cc3c7e3c883a187c15f55ba87f71a3a
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-03 13:27:59 +0530

    Navbar logout feature added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from "react-router-dom";
+
 
 const Navbar = ({ setShowLogin }) => {
 
@@ -10,6 +12,13 @@ const Navbar = ({ setShowLogin }) => {
 
     const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
 
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setToken("");
+        navigate("/")
+    }
 
     return (
         <div className='navbar'>
@@ -32,7 +41,7 @@ const Navbar = ({ setShowLogin }) => {
                         <ul className="nav-profile-dropdown">
                             <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
                             <hr />
-                            <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+                            <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
                         </ul>
                     </div>
                 }
