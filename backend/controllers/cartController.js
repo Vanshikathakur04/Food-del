ff1d1ce4102012d80509c031f31907f589fab81a
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-03 16:41:40 +0530

    Use findById for adding items to cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js"
 
 const addToCart = async (req, res) => {
 try {
-  let userData = await userModel.findOne({_id:req.body.userId})
+  let userData = await userModel.findById(req.body.userId)
   let cartData = await userData.cartData;
   if(!cartData[req.body.itemId])
   {
