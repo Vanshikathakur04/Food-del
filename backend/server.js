af6eec1bd7d602e4961e5ad760a1ea7202dcea67
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-04 16:19:59 +0530

    Add order routes to server

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 const app = express()
@@ -16,9 +17,10 @@ app.use(cors())
 connectDB();
 
 app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working")
