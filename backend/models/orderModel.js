be7516436360ca6284f4745d332daef430404354
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-04 15:59:10 +0530

    Add order model export

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose"
 
 const orderSchema = new mongoose.Schema({
-  userId:{type:String,required:true},
-  items:{type:Array,required:true},
-  amount:{type:number,required:true},
-  address:{type:Object,required:true},
-  status:{type:String,required:"Food Processing"},
-  date:{type:Date,required:Date.now()},
-  payment:{type:Boolean,required:false},
+  userId: { type: String, required: true },
+  items: { type: Array, required: true },
+  amount: { type: number, required: true },
+  address: { type: Object, required: true },
+  status: { type: String, required: "Food Processing" },
+  date: { type: Date, required: Date.now() },
+  payment: { type: Boolean, required: false },
 })
+
+const orderModel = mongoose.models.order || mongoose.model("order", orderSchema)
+export default orderModel;
