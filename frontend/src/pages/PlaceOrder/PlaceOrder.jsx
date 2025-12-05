6bb3132744678c31ca83e61e9daeb892ef1a7899
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-05 14:40:07 +0530

    Make inputs controlled

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -29,20 +29,20 @@ const PlaceOrder = () => {
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='First name' />
-          <input type="text" placeholder='Last name' />
+          <input name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name' />
+          <input name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last name' />
         </div>
-        <input type="email" placeholder='Email address' />
-        <input type="text" placeholder='Street' />
+        <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email address' />
+        <input name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' />
         <div className="multi-fields">
-          <input type="text" placeholder='City' />
-          <input type="text" placeholder='State' />
+          <input name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
+          <input name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State' />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='Zip code' />
-          <input type="text" placeholder='Country' />
+          <input name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code' />
+          <input name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
         </div>
-        <input type="text" placeholder='Phone' />
+        <input name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone' />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
