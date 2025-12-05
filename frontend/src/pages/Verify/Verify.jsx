95c150f979bb4e8fd270c510cc7d8438082cd322
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-05 16:58:30 +0530

    Set up query params for order verification

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Verify.css'
+import { useSearchParams } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Verify = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success")
+  const orderId = searchParams.get("orderId")
+  const { url } = useContext(StoreContext);
+
   return (
     <div>
-      
+
     </div>
   )
 }
