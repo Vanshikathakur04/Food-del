1709b51d0ff19a06469d169f810e895e32c4810c
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-06 13:58:12 +0530

    Add logic to fetch user orders

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MyOrders.css'
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const MyOrders = () => {
+
+  const { url, token } = useContext(StoreContext);
+  const [data, setData] = useState([]);
+
+  const fetchOrders = async () => {
+    const response = await axios.post(url + "/api/orders/userOrders", {}, { headers: { token } });
+    setData(response.data.data);
+  }
+
+  useEffect(() => {
+    if (token) {
+      fetchOrders();
+    }
+    
+  }, [token])
   return (
     <div>
-      
+
     </div>
   )
 }
