3fcef5bb807f0c6cbb5a7af1e4bc358b105e6a13
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-07 13:55:21 +0530

    Add listOrders to get all orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -72,15 +72,25 @@ const verifyOrder = async (req, res) => {
   }
 }
 
-const userOrders = async (req,res) => {
+const userOrders = async (req, res) => {
   try {
-    const orders = await orderModel.find({userId:req.body.userId});
-    res.json({success:true,data:orders})
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders })
   } catch (error) {
     console.log(error);
-    res.json =({success:true,message:"Error"})
-    
+    res.json = ({ success: true, message: "Error" })
+
   }
 }
 
-export { placeOrder, verifyOrder, userOrders }
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders })
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" })
+  }
+}
+
+export { placeOrder, verifyOrder, userOrders, listOrders }
