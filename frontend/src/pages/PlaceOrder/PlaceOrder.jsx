345d0c945f8b07c7c214b3766b21f766656a880e
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-07 13:07:12 +0530

    Changed order API route to /api/orders/place

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -40,7 +40,7 @@ const PlaceOrder = () => {
       items: orderItems,
       amount: getTotalCartAmount() + 2,
     }
-    let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
+    let response = await axios.post(url + "/api/orders/place", orderData, { headers: { token } });
     if (response.data.success) {
       const { session_url } = response.data;
       window.location.replace(session_url);
