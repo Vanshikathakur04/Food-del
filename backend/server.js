26e47cebeec30538b2e5b5bb717b3c3fd53f2c4d
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-12-07 12:52:33 +0530

    Changed /api/order to /api/orders

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
+app.use("/api/orders", orderRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working")
